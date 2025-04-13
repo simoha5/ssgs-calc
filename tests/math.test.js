9915364c9c88e70ebf1ca3bf393e840f4d690834
simoha5 <zeusssimo@gamil.com>
2025-04-13 16:50:36 +0200

    last version

---
+++ tests/math.test.js
@@ -0,0 +1,48 @@
+// a.test.js
+const { add, subtract, multiply, divide} = require('../src/math.js');
+test("Test addizione1", () => {
+    expect(add(10, 5)).toBe(15);
+});
+test("Test addizione2", () => {
+    expect(add(-10, 5)).toBe(-5);
+});
+test("Test addizione3", () => {
+    expect(add(-10, -5)).toBe(-15);
+});
+test("Test addizione4", () => {
+    expect(add(0, 0)).toBe(0);
+});
+
+test("Test sottrazione", () => {
+    expect(subtract(10, 5)).toBe(5);
+});
+test("Test sottrazione negativa", () => {
+    expect(subtract(5, 10)).toBe(-5);
+});
+test("Test sottrazione zero", () => {
+    expect(subtract(0, 0)).toBe(0);
+});
+
+test("Test moltiplicazione", () => {
+    expect(multiply(10, 5)).toBe(50);
+});
+test("Test moltiplicazione negativa", () => {
+    expect(multiply(-10, 5)).toBe(-50);
+});
+test("Test moltiplicazione zero", () => {
+    expect(multiply(0, 5)).toBe(0);
+});
+
+test("Test divisione", () => {
+    expect(divide(10, 5)).toBe(2);
+});
+test("Test divisione negativa", () => {
+    expect(divide(-10, 5)).toBe(-2);
+});
+test("Test divisione zero", () => {
+    expect(divide(0, 5)).toBe(0);
+});
+
+test("Test divisione per zero", () => {
+    expect(() => divide(10, 0)).toThrow("Errore: divisione per zero!");
+});
